d18f96a1b29d2dfcbf16b76651df775038e4513f
Lord_Detson <babanin.dima@gmail.com>
2019-09-04 14:57:46 +0300

    Created an entity User

---
+++ src/main/java/by/babanin/entity/User.java
@@ -0,0 +1,58 @@
+package by.babanin.entity;
+
+import javax.persistence.*;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "users_tbl")
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name_fld")
+    private String name;
+    @Column(name = "email_fld")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", User.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("email='" + email + "'")
+                .toString();
+    }
+}
